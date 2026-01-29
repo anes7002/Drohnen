52e642c755080bd1e1f9dd28e6d47aaf3aadc544
Anes Jakupovic <anesjakupovic3@gmail.com>
2026-01-29 11:05:25 +0100

    Move(Forward, Backward, Left, Right)

--- Backend/controls.py
+++ Backend/controls.py
@@ -15,6 +15,21 @@ class Control:
     def land(self):
         self.drone.flight.land()
 
+    #Bewegen
+    def forward(self, distance=50):
+        self.drone.flight.forward(distance)
+
+    def backward(self, distance=50):
+        self.drone.flight.backward(distance)
+y
+    def left(self, distance=50):
+        self.drone.flight.left(distance)
+
+    def right(self, distance=50):
+        self.drone.flight.right(distance)
+
     
+
+
 
     
