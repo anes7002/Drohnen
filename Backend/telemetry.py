ef7a7e57eebe2527db4f95e2399e3f195fd9d461
oscarala <oscarapager@gmail.com>
2026-02-25 08:27:46 +0100

    single telemetry data

--- Backend/telemetry.py
+++ Backend/telemetry.py
@@ -11,3 +11,45 @@ def get_all_telemetry(self):
         "yaw": self.drone.get_yaw(),
         "flight_time": self.drone.get_flight_time(),
     }
+
+def get_battery(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_battery()
+
+def get_height(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_height()
+
+def get_speed(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_speed()
+
+def get_pitch(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_pitch()
+
+def get_roll(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_roll()
+
+def get_yaw(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_yaw()
+
+def get_flight_time(self):
+    if not self.connected:
+        raise Exception("Drone is not connected")
+    
+    return self.drone.get_flight_time()
