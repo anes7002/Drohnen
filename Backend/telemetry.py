36294749c8fb8737f541e11cabddf69062cdf6e8
Diego Felzmann <diego.felzmann@htl-saalfelden.at>
2026-03-04 08:49:50 +0100

    Refactor Telemetry class methods for improved structure and readability

--- Backend/telemetry.py
+++ Backend/telemetry.py
@@ -1,55 +1,57 @@
-def get_all_telemetry(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return {
-        "battery": self.drone.get_battery(),
-        "height": self.drone.get_height(),
-        "speed": self.drone.get_speed(),
-        "pitch": self.drone.get_pitch(),
-        "roll": self.drone.get_roll(),
-        "yaw": self.drone.get_yaw(),
-        "flight_time": self.drone.get_flight_time(),
-    }
+class Telemetry:
 
-def get_battery(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_battery()
+    def get_all_telemetry(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return {
+            "battery": self.drone.get_battery(),
+            "height": self.drone.get_height(),
+            "speed": self.drone.get_speed(),
+            "pitch": self.drone.get_pitch(),
+            "roll": self.drone.get_roll(),
+            "yaw": self.drone.get_yaw(),
+            "flight_time": self.drone.get_flight_time(),
+        }
 
-def get_height(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_height()
+    def get_battery(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_battery()
 
-def get_speed(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_speed()
+    def get_height(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_height()
 
-def get_pitch(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_pitch()
+    def get_speed(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_speed()
 
-def get_roll(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_roll()
+    def get_pitch(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_pitch()
 
-def get_yaw(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_yaw()
+    def get_roll(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_roll()
 
-def get_flight_time(self):
-    if not self.connected:
-        raise Exception("Drone is not connected")
-    
-    return self.drone.get_flight_time()
+    def get_yaw(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_yaw()
+
+    def get_flight_time(self):
+        if not self.connected:
+            raise Exception("Drone is not connected")
+        
+        return self.drone.get_flight_time()
